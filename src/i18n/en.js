44201859507c12e2105c14dbf3b2a4892b190d76
a6917 <a69173894@yahoo.com>
2025-04-28 00:57:07 +0800

    en.js json格式化

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -820,7 +820,7 @@ export default {
     "To ensure convenience for our investors, PBK Miner provides a diverse range of payment methods for both depositing funds and receiving payments.",
     "CLOUD MINING INVESTMENT IN TEXAS",
     "<p class=\"mt-2 s-content\"><span class=\"bullet-icon\">•</span>Welcome to {WT},your ticket to the exciting world of cryptocurrency mining.Whether you're a novice or a seasoned investor,our platform offers an array of opportunities to explore and profit from the digital currency realm.</p><p class=\"mt-2 s-content\" ><span class=\"bullet-icon\" >•</span>In the heart of Texas,{WT}'s journey begins,leveraging the state's vast resources and technological prowess to deliver cutting-edge cloud mining solutions.</p><p class=\"mt-2 s-content\" ><span class=\"bullet-icon\" >•</span>At {WT},we're not just about mining;we're about innovation. We provide Cloud Mining Investment in the USA,ensuring you stay at the forefront of cryptocurrency trends.</p><p class=\"mt-2 s-content\" ><span class=\"bullet-icon\" >•</span>Our Cloud Mining Hardware in Washington boasts top-notch equipment,guaranteeing optimal performance and reliability. Whether you're interested in Bitcoin or other cryptocurrencies,we have the hardware to support your goals.</p><p class=\"mt-2 s-content\"><span class=\"bullet-icon\">•</span>Say goodbye to complex setups;with Cloud Mining Bitcoin in California,you can access our services online.Our Bitcoin Mining Cloud Service is designed with user-friendliness in mind,catering to both beginners and experts.</p><p class=\"mt-2 s-content\"><span class=\"bullet-icon\">•</span>We take your security seriously,offering Reliable Cloud Mining Services backed by robust measures.{WT} is known for its transparency and trustworthiness,making us a preferred choice for cloud mining,not just in Arizona,but across the USA.</p><p class=\"mt-2 s-content\"><span class=\"bullet-icon\">•</span>Our dedication to blockchain technology is evident in our Blockchain Cloud Mining in the USA.We utilize public blockchains for enhanced mining experiences,and our Enhanced Cloud Mining Security ensures the safety of your assets.</p><p class=\"mt-2 s-content\"><span class=\"bullet-icon\">•</span>From Litecoin to Ethereum,we've got you covered. Explore various options,such as Litecoin Cloud Mining in Massachusetts and Ethereum Cloud Mining in Colorado,to diversify your investment portfolio.</p><p class=\"mt-2 s-content\" >But it doesn't end there.{WT} offers a unique opportunity for you to profit from your passion.Join our Cloud Mining Affiliate Programs and Crypto Affiliate Program in the USA,turning your interest in cryptocurrency into a source of income.</p><p class=\"mt-2 s-content\"><span class=\"bullet-icon\">•</span>With {WT},your journey into the cryptocurrency universe begins.Start mining,start earning,and start securing your financial future today.</p>",
-    "29",
+    "29"
   ],
   "latest": [
     "Latest Transactions",
@@ -912,7 +912,6 @@ export default {
   "complete":"completed",
   "cancel": "Cancel",
   "confirm": "Confirm",
-  // 新-about.vue页面业务特色卡片模块
   "about_us_adv": {
     "title": "Next-Gen Mining: Smarter, Faster, More Profitable",
     "text": "Harness cutting-edge technology and robust infrastructure to make mining effortless, efficient, and profitable.",
@@ -957,15 +956,14 @@ export default {
     {
       "title": "Solar Energy Cycle",
       "content": ""
-    },
+    }
   ],
-  // 原本写死的文案
   "i18nLan": {
     "about_us": {
       "text1":"PBK FINANCIAL SERVICES LTD",
       "text2":"30 Colston Avenue, Carshalton, Surrey, England, SM5 2NU",
       "text3":"Private limited Company",
-      "text4":"21 October 2019",
+      "text4":"21 October 2019"
     },
     "newticket": {
       "text1": "Open New Support Ticket",
@@ -981,10 +979,10 @@ export default {
       "text11": "Attachments",
       "text12": "Add More",
       "text13": " Allowed File Extensions: .jpg, .jpeg, .png, .pdf, .doc, .docx",
-      "text14": "Submit",
+      "text14": "Submit"
     },
     "product": {
-      "text1": "USDT",
-    },
-  },
+      "text1": "USDT"
+    }
+  }
 }
